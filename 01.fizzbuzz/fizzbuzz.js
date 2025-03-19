d4b1f480585f3f850990bab0694ad3b8ba09515f
Kyohei <kyoheisato0410@gmail.com>
2025-03-19 22:22:28 +0900

    fizzbuzzの課題作成

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 100; i++) {
+    let str = "";
+    if ((i % 3) === 0) str += "Fizz";
+    if ((i % 5) === 0) str += "Buzz";
+    if (str === "") str += i.toString();
+    console.log(str);
+}
+
