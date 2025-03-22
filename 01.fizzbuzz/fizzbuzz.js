88786b75f73475f7e638f2a4ef87b22fc63c17b5
Kyohei <kyoheisato0410@gmail.com>
2025-03-22 12:51:20 +0900

    フォーマッティングの適用

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 for (let i = 1; i <= 100; i++) {
-    let str = "";
-    if ((i % 3) === 0) str += "Fizz";
-    if ((i % 5) === 0) str += "Buzz";
-    if (str === "") str += i.toString();
-    console.log(str);
+  let str = "";
+  if (i % 3 === 0) str += "Fizz";
+  if (i % 5 === 0) str += "Buzz";
+  if (str === "") str += i.toString();
+  console.log(str);
 }
-
