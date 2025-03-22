fc2ba523cedc2a8a72672462a7458fa090c00990
Kyohei <kyoheisato0410@gmail.com>
2025-03-22 12:52:56 +0900

    if文処理にて、明示的にブロックを使用するように変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,8 +2,14 @@
 
 for (let i = 1; i <= 100; i++) {
   let str = "";
-  if (i % 3 === 0) str += "Fizz";
-  if (i % 5 === 0) str += "Buzz";
-  if (str === "") str += i.toString();
+  if (i % 3 === 0) {
+    str += "Fizz";
+  }
+  if (i % 5 === 0) {
+    str += "Buzz";
+  }
+  if (str === "") {
+    str += i.toString();
+  }
   console.log(str);
 }
