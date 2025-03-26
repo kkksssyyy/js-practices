c830abce09fd90346f8b60ddc643226ac7c02a1f
Kyohei <kyoheisato0410@gmail.com>
2025-03-26 21:57:53 +0900

    カレンダーが出力されるように修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,7 +1,57 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
+import { DateTime } from "luxon";
 
 const args = minimist(process.argv.slice(2));
 
-console.log(args);
+const y = createYear(args["y"]);
+const m = createMonth(args["m"]);
+const m_padded = String(m).padStart(2, "0");
+
+// 指定された年月の最初の日を生成
+const dateString = `${y}-${m_padded}-01`;
+const firstDay = DateTime.fromISO(dateString, { zone: "Asia/Tokyo" });
+
+let targetDay = firstDay;
+let bodyString = "";
+
+// 最初の週が右詰めになるようにスペースを追加します
+if (targetDay.weekday !== 7) {
+  for (let i = 1; i <= targetDay.weekday; i++) {
+    bodyString += "   ";
+  }
+}
+
+do {
+  bodyString += String(targetDay.day).padStart(2, " ");
+  // 見やすくするためのスペースを追加
+  bodyString += " ";
+  // 土曜の場合は改行を追加
+  if (targetDay.weekday === 6) {
+    bodyString += "\n";
+  }
+  targetDay = targetDay.plus({ days: 1 });
+} while (targetDay.month === firstDay.month);
+
+const hedderString = `      ${m}月 ${y}        
+日 月 火 水 木 金 土  `;
+console.log(hedderString);
+console.log(bodyString);
+
+function createYear(y) {
+  if (y === undefined) {
+    const today = DateTime.now().setZone("Asia/Tokyo");
+    return today.year;
+  } else {
+    return y;
+  }
+}
+function createMonth(m) {
+  if (m === undefined) {
+    const today = DateTime.now().setZone("Asia/Tokyo");
+    return today.month;
+  } else {
+    return m;
+  }
+}
