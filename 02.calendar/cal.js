26dae69e36569e00a2303640b770ca4f7cb0cddd
Kyohei <kyoheisato0410@gmail.com>
2025-04-22 20:43:11 +0900

    日にちの先頭に不要なスペースが入るのを修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -51,12 +51,13 @@ if (targetDay.weekday !== 7) {
 }
 
 do {
-  // 見やすくするためのスペースを追加
-  bodyString += " ";
   bodyString += String(targetDay.day).padStart(2, " ");
   if (targetDay.weekday === 6) {
     // 土曜の場合は改行を追加
     bodyString += "\n";
+  } else {
+    // 見やすくするためのスペースを追加
+    bodyString += " ";
   }
   targetDay = targetDay.plus({ days: 1 });
 } while (targetDay.month === firstDay.month);
