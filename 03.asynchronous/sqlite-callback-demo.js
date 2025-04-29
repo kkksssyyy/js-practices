c63dbc9b7662c8c61beabeed36767a282cc4741d
Kyohei <kyoheisato0410@gmail.com>
2025-04-29 15:17:53 +0900

    テーブルの削除を忘れていたので追記

--- 03.asynchronous/sqlite-callback-demo.js
+++ 03.asynchronous/sqlite-callback-demo.js
@@ -3,46 +3,45 @@
 import sqlite3 from "sqlite3";
 import timers from "timers/promises";
 
-const db1 = new sqlite3.Database(":memory:", function (err) {
-  db1.run(
-    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)",
-    function (err) {
-      db1.run("insert into books(title) values(?)", "title1", function (err) {
-        console.log(this.lastID);
-        db1.get(
-          "select * from books where id=(?)",
-          this.lastID,
-          function (err, row) {
-            console.log(row);
-            db1.close();
-          },
-        );
-      });
-    },
-  );
-});
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)",
+  function (err) {
+    db.run("insert into books(title) values(?)", "title1", function (err) {
+      console.log(this.lastID);
+      db.get(
+        "select * from books where id=(?)",
+        this.lastID,
+        function (err, row) {
+          console.log(row);
+          db.run("DROP TABLE books");
+        },
+      );
+    });
+  },
+);
 
 await timers.setTimeout(100);
 
-const db2 = new sqlite3.Database(":memory:", function (err) {
-  db2.run(
-    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)",
-    function (err) {
-      db2.run("insert into hoge(title) values(?)", "title1", function (err) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(this.lastID);
-        db2.get(
-          "select * from books where id=(?)",
-          this.lastID,
-          function (err, row) {
-            console.log(row);
-            db2.close();
-          },
-        );
-      });
-    },
-  );
-});
+db.run(
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)",
+  function (err) {
+    // エラーを発生させる
+    db.run("insert into hoge(title) values(?)", "title1", function (err) {
+      if (err) {
+        console.error("Error occurred:", err.message);
+        return;
+      }
+      console.log(this.lastID);
+      db.get(
+        "select * from books where id=(?)",
+        this.lastID,
+        function (err, row) {
+          console.log(row);
+          db.run("DROP TABLE books");
+        },
+      );
+    });
+  },
+);
